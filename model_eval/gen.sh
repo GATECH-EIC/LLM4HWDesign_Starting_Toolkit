f56ee39276069711557af5e07cc3a7dc1d0dd541
yuzz1020 <yuzhongzhi1994@126.com>
2024-08-08 23:26:45 -0700

    update eval w/ merged model

--- model_eval/gen.sh
+++ model_eval/gen.sh
@@ -1,29 +1,22 @@
-#should be from huggingface hub;
-#base_model="codellama/CodeLlama-7b-Instruct-hf"
-base_model=$1
-#tokenizer_type="code_llama"
-tokenizer_type=$2
-
-
-work_dir=$(pwd)
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64'
 
 accelerate launch --multi_gpu generate.py \
-    --checkpoint_dir ./result_ckpt \
+    --checkpoint_dir PATH_TO_THE_FOLDER_WITH_MODEL_AND_CONFIG_FILE \
     --model_type "qlora" \
-    --base_model $base_model \
-    --tokenizer_type $tokenizer_type \
-    --cache_dir $work_dir/.cache \
-    --hf_token $HF_TOKEN \
+    --base_model "codellama/CodeLlama-7b-Instruct-hf" \
+    --tokenizer_type "code_llama" \
+    --cache_dir "./HF_cache/" \
+    --hf_token "INPUT_YOUR_TOKEN_HERE" \
     --max_new_tokens 1024 \
     --temperature 0.7 \
-    --desc_file ../verilog-eval/descriptions/VerilogDescription_Machine.jsonl \
+    --desc_file ../verilog_eval/descriptions/VerilogDescription_Machine.jsonl \
     --desc_key "detail_description" \
     --prompt_type "baseline" \
-    --eval_file ../verilog-eval/data/VerilogEval_Machine.jsonl \
+    --eval_file ../verilog_eval/data/VerilogEval_Machine.jsonl \
     --output_file ./data/gen.jsonl \
     --fp16 \
     --sample_k 20 \
     --result_name Test \
-    --batch_size 2 
+    --batch_size 1
